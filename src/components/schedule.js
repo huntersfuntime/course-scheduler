351cf10c55069def62abba21a40679ec7ee5b19a
huntersfuntime <hunterorges@gmail.com>
2018-04-25 16:03:45 -0600

    Added other course info, and styles for class schedule

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -14,7 +14,7 @@ class Schedule extends Component {
         return (
             <div key={this.props.courses.indexOf(course)} className={`slot ${course.enrolled ? 'slot__course' : 'slot__empty'}`}>
                 <div className="slot__title">{course.enrolled ? course.title : 'Empty Slot'}</div>
-                <a className={`action slot__remove ${course.enrolled ? 'show-content' : 'hide-content'}`} onClick={() => this.props.removeCourse(course)}>remove course</a>
+                <a className={`action slot__remove`} onClick={() => this.props.removeCourse(course)}>remove course</a>
             </div>
         )
     }
